7984d74022aed06f1c02d462807fbb9ab1d1476b
GHKim <aass98998@snu.ac.kr>
2018-09-10 01:32:50 +0900

    can choose seperate or append mode

--- pma2tif.m
+++ pma2tif.m
@@ -1,4 +1,4 @@
-function tif_name = pma2tif(file_name)
+function tif_name = pma2tif(file_name, mode)
 
     fid_pma = fopen(file_name,'r');
     file_info=dir(file_name);
@@ -9,16 +9,36 @@ function tif_name = pma2tif(file_name)
     film_length=(file_info.bytes-4)/xsize/ysize;
     tif_name = strcat(file_name(1:end-4),'.tif');
     
-    one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
-    imwrite(one_frame', tif_name);
-
-for i=1:film_length-1
-    if rem(i,100) == 0
+    if mode == 1
+        foldername = strcat('Tiff',file_name(1:end-4));
+        c = cd();
+        addpath(c);
+        mkdir(foldername)
+        cd(foldername)
         
-        disp([num2str(i+1), '/', num2str(film_length)]);
-    end
-    one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
-    imwrite(one_frame', tif_name, 'WriteMode', 'append');  
-end
+        for i=1:film_length
+            if rem(i,100) == 0
+                disp([num2str(i+1), '/', num2str(film_length)]);
+            end
+            one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+            name_i = sprintf('%05d',i);
+            tif_name_i = strcat(file_name(1:end-4),'_',name_i,'.tif');
+            imwrite(one_frame, tif_name_i);  
+        end
+    elseif mode == 2
+        one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+        imwrite(one_frame', tif_name);
 
+        for i=1:film_length-1
+            if rem(i,100) == 0
+
+                disp([num2str(i+1), '/', num2str(film_length)]);
+            end
+            one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+            imwrite(one_frame, tif_name, 'WriteMode', 'append');  
+        end
+    else
+        tif_name = nan;
+    end
+    fclose('all');
 end
