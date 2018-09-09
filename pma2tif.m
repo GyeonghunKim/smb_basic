4077bb195768904deb2f655cc8fe5a73b80d77ff
GHKim <aass98998@snu.ac.kr>
2018-09-09 23:54:02 +0900

    transform .pma file to .tif file

--- pma2tif.m
+++ pma2tif.m
@@ -1,3 +1,24 @@
-function tif = pma2tif(pmafile)
-   
+function tif_name = pma2tif(file_name)
+
+    fid_pma = fopen(file_name,'r');
+    file_info=dir(file_name);
+
+    ysize=fread(fid_pma,1,'int16'); 
+    xsize=fread(fid_pma,1,'int16');
+    
+    film_length=(file_info.bytes-4)/xsize/ysize;
+    tif_name = strcat(file_name(1:end-4),'.tif');
+    
+    one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+    imwrite(one_frame', tif_name);
+
+for i=1:film_length-1
+    if rem(i,100) == 0
+        
+        disp([num2str(i+1), '/', num2str(film_length)]);
+    end
+    one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+    imwrite(one_frame', tif_name, 'WriteMode', 'append');  
+end
+
 end
