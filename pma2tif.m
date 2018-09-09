e67a0fdc69e0e51b7c0f87c2661a61159794be3f
GHKim <aass98998@snu.ac.kr>
2018-09-10 01:42:12 +0900

    fix some data-type bug

--- pma2tif.m
+++ pma2tif.m
@@ -20,13 +20,13 @@ function tif_name = pma2tif(file_name, mode)
             if rem(i,100) == 0
                 disp([num2str(i+1), '/', num2str(film_length)]);
             end
-            one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+            one_frame = uint16(fread(fid_pma,[ysize,xsize], 'uint8'));
             name_i = sprintf('%05d',i);
             tif_name_i = strcat(file_name(1:end-4),'_',name_i,'.tif');
-            imwrite(one_frame, tif_name_i);  
+            imwrite(one_frame', tif_name_i);  
         end
     elseif mode == 2
-        one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
+        one_frame = uint16(fread(fid_pma,[ysize,xsize], 'uint8'));
         imwrite(one_frame', tif_name);
 
         for i=1:film_length-1
@@ -34,8 +34,8 @@ function tif_name = pma2tif(file_name, mode)
 
                 disp([num2str(i+1), '/', num2str(film_length)]);
             end
-            one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
-            imwrite(one_frame, tif_name, 'WriteMode', 'append');  
+            one_frame = uint16(fread(fid_pma,[ysize,xsize], 'uint8'));
+            imwrite(one_frame', tif_name, 'WriteMode', 'append');  
         end
     else
         tif_name = nan;
